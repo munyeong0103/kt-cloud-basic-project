ed8160593281cc0384ff7c35a2a5bc8f6b663b7f
munyeong0103 <skymy02@ajou.ac.kr>
2025-10-30 11:51:59 +0900

    feat: added swagger config

--- src/main/java/com/kt/controller/UserController.java
+++ src/main/java/com/kt/controller/UserController.java
@@ -9,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kt.dto.UserCreateRequest;
 import com.kt.service.UserService;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@ApiResponses(value = {
+	@ApiResponse(responseCode = "400", description = "유효성 검사"),
+	@ApiResponse(responseCode = "500", description = "서버 에러"),
+})
+@Tag(name = "유저")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
@@ -21,7 +30,7 @@ public class UserController {
 	//loginId, password, name, birthday
 	//json 형태의 body에 담겨서 post 요청으로 /uSERS로 들어오면
 	//@RequestBody를 보고 jackson object mapper가 동작해서 json을 읽어서 dto로 변환
-	public void create(@RequestBody UserCreateRequest request) {
+	public void create(@Valid @RequestBody UserCreateRequest request) {
 		userService.create(request);
 	}
 }
--- src/main/java/com/kt/dto/UserCreateRequest.java
+++ src/main/java/com/kt/dto/UserCreateRequest.java
@@ -4,13 +4,26 @@ import java.time.LocalDateTime;
 
 import com.kt.domain.Gender;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record UserCreateRequest(
+	@NotBlank
 	String loginId,
+	@NotBlank
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^])[A-Za-z\\d!@#$%^]{8,}$")
 	String password,
+	@NotBlank
 	String name,
+	@NotBlank
 	String email,
+	@NotBlank
+	@Pattern(regexp = "^(0\\d{1,2})-(\\d{3,4})-(\\d{4})$")
 	String mobile,
+	@NotNull
 	Gender gender,
+	@NotNull
 	LocalDate birthday,
 	LocalDateTime createdAt,
 	LocalDateTime updatedAt
