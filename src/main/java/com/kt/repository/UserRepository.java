49a70fa5f1f90c04c5a0f6b4f44648fc64bb4004
munyeong0103 <skymy02@ajou.ac.kr>
2025-10-30 11:05:21 +0900

    feat: updated user repository

--- src/main/java/com/kt/repository/UserRepository.java
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -13,8 +13,9 @@ public class UserRepository {
 	private final JdbcTemplate jdbcTemplate;
 
 	public void save(User user) {
-		var sql = "INSERT INTO MEMBER (loginId, password, name, birthday) VALUES (?, ?, ?, ?)";
-		jdbcTemplate.update(sql, user.getLoginId(), user.getPassword(), user.getName(), user.getBirthday());
+		var sql = "INSERT INTO MEMBER (id, loginId, password, name, email, mobile, gender, birthday, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		jdbcTemplate.update(sql, user.getId(), user.getLoginId(), user.getPassword(), user.getName(), user.getEmail(),
+			user.getMobile(), user.getGender().name(), user.getBirthday(), user.getCreatedAt(), user.getUpdatedAt());
 	}
 
 	public long selectMaxId(){
